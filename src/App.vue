006895c7e1f21c5ed470770abd746a0e314cb606
Rafli Ramadhan <rafliramdhn@gmail.com>
2020-08-29 22:07:12 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,102 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="d-flex container justify-content-between mt-3">
+      <div class="brand-logo">Nyuss</div>
+      <ul class="navbar-nav flex-row">
+        <li class="nav-item mx-3">
+          <a href class="nav-link">Promo</a>
+        </li>
+        <li class="nav-item mx-3">
+          <a href class="nav-link">Unggulan</a>
+        </li>
+        <li class="nav-item mx-3">
+          <a href class="nav-link">Menu</a>
+        </li>
+        <li class="nav-item mx-3">
+          <a href class="nav-link">Kontak</a>
+        </li>
+      </ul>
+      <a href="#" class="btn btn-primary pb-0 m-0 px-4 login-bottom">Login</a>
+    </div>
+    <!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+        <a class="navbar-brand" href="#">Nyuss</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav navbar-center">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Promo</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Unggulan</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Menu</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Kontak</a>
+            </li>
+          </ul>
+
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </div>
+      </div>
+    </nav>-->
+    <router-view />
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
+.bg-primary {
+  background-color: #513ba9;
+}
+a.login-bottom {
+  font-size: 18px;
+  font-weight: 700;
+  background-color: #513ba9;
+}
+
+.navbar-nav.navbar-center {
+  position: absolute;
+  left: 50%;
+  transform: translatex(-50%);
+}
+.nav-link {
+  color: black;
+  font-size: 18px;
+}
+.brand-logo {
+  font-weight: 700;
+  font-size: 30px;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Quicksand", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="container">
+    <div class="d-flex justify-content-between flex-row mt-5">
+      <!-- <div style="width:540px;height:400px">test</div> -->
+      <img src="https://picsum.photos/540/400" alt srcset />
+
+      <div class="d-flex flex-column justify-content-between">
+        <img src="https://picsum.photos/255/185" alt srcset />
+        <img src="https://picsum.photos/255/185" alt srcset />
+        <!-- <div style="width:255;height:185px">test</div>
+        <div style="width:255;height:185px">test</div>-->
+      </div>
+      <img src="https://picsum.photos/255/400" alt srcset />
+    </div>
+
+    <div class="mt-5">
+      <div class="unggulan text-left">
+        <div class="unggulan-text">Produk Unggulan Kami</div>
+        <div class="unggulan-subtext">Coba dulu aja siapa tau suka kan.</div>
+      </div>
+      <div class="d-flex justify-content-between mt-4">
+        <img v-for="kelas in 4" :key="kelas.index" src="https://picsum.photos/255/400" alt srcset />
+      </div>
+    </div>
+    <div class="mt-5">
+      <div class="menu">
+        <div class="menu-text text-center">Menu</div>
+        <div class="menu-subtext text-center">Pilih sesuai selera kamu.</div>
+        <div class="menu-category my-4">
+          <button
+            class="btn btn-primary px-5 mr-4 py-0"
+            v-for="category in 4"
+            :key="category.index"
+          >Test</button>
+        </div>
+      </div>
+      <div class="d-flex justify-content-between flex-wrap mt-4">
+        <img
+          v-for="kelas in 9"
+          :key="kelas.index"
+          style="margin-bottom:30px"
+          src="https://picsum.photos/255/255"
+          alt
+          srcset
+        />
+      </div>
+    </div>
+    <div class="footer my-4 bg-primary">
+      <div class="d-flex justify-content-between">
+        <div style="width:317px">
+          <div class="brand-logo">Nyuss</div>
+          <div>
+            Minuman yang bikin nyuss. bikin seleb
+            bilang “Rasanya mau meninggal”.
+          </div>
+          <div class="info">
+            <div class="contact">contact@nyuss.com</div>
+            <div class="telp">+62 812 8192 5236</div>
+          </div>
+        </div>
+        <div style="width:317px">
+          <div class="footer-help">Sales Support</div>
+          <div>
+            Ingin membuat mitra atau kerjasama
+            dengan kami? silakan hubungi.
+          </div>
+          <div class="info">
+            <div class="contact">contact@nyuss.com</div>
+            <div class="telp">+62 812 8192 5236</div>
+          </div>
+        </div>
+        <div style="width:317px">
+          <div class="footer-help">Tech Support</div>
+          <div>
+            Terdapat kesalahan pada sistem kami?
+            silakan hubungi.
+          </div>
+          <div class="info">
+            <div class="contact">contact@nyuss.com</div>
+            <div class="telp">+62 812 8192 5236</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home"
+};
+</script>
+
+<style scoped>
+.menu-category button {
+  border-radius: 40px;
+  font-size: 18px;
+  font-size: 400;
+  background-color: #513ba9;
+}
+.footer-help {
+  margin-top: 8px;
+  font-size: 24px;
+  font-weight: 700;
+}
+.footer {
+  color: white;
+}
+img {
+  border-radius: 8px;
+}
+.unggulan .unggulan-text,
+.menu .menu-text {
+  font-size: 36px;
+  font-weight: 700;
+}
+.unggulan .unggulan-subtext,
+.menu .menu-subtext {
+  font-size: 24px;
+}
+</style>
