7249ec13b129378ee08a56fd3bd06b2e8dd56064
Rafli Ramadhan <rafliramdhn@gmail.com>
2020-09-06 01:42:16 +0700

    remove old navbar

--- src/App.vue
+++ src/App.vue
@@ -18,41 +18,6 @@
       </ul>
       <a href="#" class="btn btn-primary pb-0 m-0 px-4 login-bottom">Login</a>
     </div>
-    <!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container">
-        <a class="navbar-brand" href="#">Nyuss</a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav navbar-center">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Promo</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Unggulan</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Menu</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Kontak</a>
-            </li>
-          </ul>
-
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </div>
-      </div>
-    </nav>-->
     <router-view />
   </div>
 </template>
