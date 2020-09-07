b4fca1421e0bad612aedb36a291f32129d2bd31f
Rafli Ramadhan <rafliramdhn@gmail.com>
2020-09-07 17:27:55 +0700

    add several database

---
+++ database/migrations/2020_09_07_080940_create_transaksis_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransaksisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaksis', function (Blueprint $table) {
+            $table->id();
+            // id_produk here 
+            $table->String("id_pelanggan");
+            $table->String("jumlah_produk");
+            $table->String("total_harga");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksis');
+    }
+}
